a8d2f7613133109c49503ef8cb0bac61c13efadd
gem-ankitbisht <ankit.bisht@geminisolutions.com>
2023-03-03 16:55:53 +0530

    index page added

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        TODContext _context = new TODContext();
         public ActionResult Index()
         {
-            return View();
+            var listofData = _context.Persons.ToList();
+            return View(listofData);
         }
 
         public ActionResult About()
