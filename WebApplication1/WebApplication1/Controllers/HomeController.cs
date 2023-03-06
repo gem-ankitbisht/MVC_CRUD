fdcb010f1b567b7e2a370fc24f079d3994f28bd1
gem-ankitbisht <ankit.bisht@geminisolutions.com>
2023-03-06 16:09:11 +0530

    edit and deleted added to project

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -15,12 +15,50 @@ namespace WebApplication1.Controllers
             var listofData = _context.Persons.ToList();
             return View(listofData);
         }
-
-        public ActionResult About()
+        [HttpGet]
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+        [HttpPost]
+        public ActionResult Create( Person model)
+        {
+            _context.Persons.Add(model);
+            _context.SaveChanges();
+            ViewBag.message = "Data Inserted successfully";
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var data = _context.Persons.Where(x => x.PersonID == id).FirstOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Person model)
+        {
+            var data = _context.Persons.Where(x=>x.PersonID == model.PersonID).FirstOrDefault();
+            if(data != null)
+            {
+                data.PersonID = model.PersonID;
+                data.FirstName = model.FirstName;   
+                data.LastName = model.LastName;
+                data.Address = model.Address;
+                data.City = model.City;
+                _context.SaveChanges();
+               
+            }
+            return RedirectToAction("index");
+        }
+        public ActionResult delete(int id)
+        {
+            var data = _context.Persons.Where(x => x.PersonID == id).FirstOrDefault();
+            _context.Persons.Remove(data);
+            _context.SaveChanges();
+            return RedirectToAction("index");
         }
 
         public ActionResult Contact()
